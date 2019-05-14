0b487ed5ac96af8a456b08ff68d9259555ab8131
Cheng Long Liew <s3517028@student.rmit.edu.au>
2019-05-14 14:49:32 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+  
   def guest
   end
+  
+  def course
+  end 
+  
+  def coordinator
+  end   
+  
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Course Management App"
+  end
+  
+   test "should get root" do
+    get '/'
+    assert_response :success
+  end
+  
   test "should get guest" do
     get static_pages_guest_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+  
+  test "should get course" do
+    get static_pages_course_url
+    assert_response :success
+    assert_select "title", "Courses | #{@base_title}"
+  end
+  
+  test "should get coordinator" do
+    get static_pages_coordinator_url
+    assert_response :success
+    assert_select "title", "Coordinators | #{@base_title}"
   end
 
 end
